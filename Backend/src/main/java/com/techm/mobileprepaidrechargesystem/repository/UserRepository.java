112cfee1e59575b2030b93c7e05458deb1dbb67f
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-04-03 23:38:12 +0530

    changes in repository classes

--- Backend/src/main/java/com/techm/mobileprepaidrechargesystem/repository/UserRepository.java
+++ Backend/src/main/java/com/techm/mobileprepaidrechargesystem/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.techm.mobileprepaidrechargesystem.repository;
 
 import com.techm.mobileprepaidrechargesystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserPhonenumber(String userPhonenumber);
 	Optional<User> findByUsername(String username);
