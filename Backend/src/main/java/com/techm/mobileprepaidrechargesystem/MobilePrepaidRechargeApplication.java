a877ca0f69eae9af7d38bf86b2af5246f969ba06
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-04-03 23:34:49 +0530

    changes in repository classes

--- Backend/src/main/java/com/techm/mobileprepaidrechargesystem/MobilePrepaidRechargeApplication.java
+++ Backend/src/main/java/com/techm/mobileprepaidrechargesystem/MobilePrepaidRechargeApplication.java
@@ -2,8 +2,10 @@ package com.techm.mobileprepaidrechargesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.techm.mobileprepaidrechargesystem.repository")
 public class MobilePrepaidRechargeApplication {
 
 	public static void main(String[] args) {
